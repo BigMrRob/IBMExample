431e90b81bc4ae66b0e7a8b7ae02ecf3e01aa8f0
Robert B Farris <bigmrrob@Roberts-MacBook-Pro.local>
2021-12-16 13:35:19 -0600

    finished lab

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var principal = document.getElementById("principal").value;
+
+  if (principal < 0) {
+    alert("Principal must be positive");
+    principal.focus();
+    return;
+  }
+
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = (principal * rate * years) / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+
+  document.getElementById("result").innerHTML =
+    "After " +
+    years +
+    " years at " +
+    rate +
+    "% interest, the investment will be worth $" +
+    (principal + interest) +
+    ".";
 }
-        
+
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerHTML = rateval;
+}
